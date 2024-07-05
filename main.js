d093afffa2e3be7f9b8eed88756b1ebbf9c76dd3
ayewhosthat <alan.notiverson@gmail.com>
2024-07-04 21:47:21 -0400

    Get valid moves for a given position on the board

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ const BOARDWIDTH = 7;
 // create arrays that correspond to all our 'possible' moves if we were in the centre of the board
 const deltaX = [-2, -2, -1, -1, 1, 1, 2, 2];
 const deltaY = [-1, 1, -2, 2, -2, 2, -1, 1];
+
+function getValidMoves(position) {
+    const validMoves = [];
+    const currX = position[0];
+    const currY = position[1];
+    for (let i = 0; i <= BOARDHEIGHT; i++) {
+        const newX = currX + deltaX[i];
+        const newY = currY + deltaY[i];
+        if (newX >= 0 && newX <= BOARDWIDTH && newY >= 0 && newY <= BOARDHEIGHT) {
+            validMoves.push([newX, newY]);
+        }
+    }
+    return validMoves;
+}
